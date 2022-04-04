9cba04f778c2d8052d37f8bf75a0047ede4223cc
Sydney Anderson <sydney-anderson@uiowa.edu>
2022-04-04 16:11:30 -0500

    added picture

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', pageTitle='About VTM')
+    return render_template('about.html')
 
 @app.route('/estimate')
 def estimate():
-    return render_template('estimate.html', pageTitle='Create an Estimate')
+    return render_template('estimate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
