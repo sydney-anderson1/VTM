13dfca76846b0ee328702f63fc7cf51c5f316b44
Sydney Anderson <sydney-anderson@uiowa.edu>
2022-04-19 15:38:32 -0500

    added calculations

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def TotalCost(height, radius):
+    pi = 3.14
+    areaTankTop = pi*(radius**2)
+    areaTankSide = 2*pi*(radius*height)
+    areaTotal = areaTankTop + areaTankSide
+    areaSqFt = areaTotal/144
+    materialCostTotal = areaSqFt*25
+    laborCostTotal = areaSqFt*15
+    TotalCostEst = materialCostTotal + laborCostTotal
+    return TotalCostEst
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,6 +26,18 @@ def about():
 
 @app.route('/estimate')
 def estimate():
+    return render_template('estimate.html')
+
+@app.route('/estimate', methods=['POST'])
+def add_inputs():
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius']
+        height = float(form['height'])
+
+        tankCost = TotalCost(height, radius)
+        
+        return render_template('estimate.html', pageTitle='Estimate', estimate=Cost)
     return render_template('estimate.html')
 
 if __name__ == '__main__':
